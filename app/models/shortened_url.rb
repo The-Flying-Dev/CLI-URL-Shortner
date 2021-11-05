176b4026cf080576e84c8695281af8d97a1a8249
Leandran <leandranm@outlook.com>
2021-11-05 14:36:24 +0200

    add factory and random_code methods to ShortenedUrl class

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -6,5 +6,26 @@ class ShortenedUrl < ApplicationRecord
       primary_key: :id,
       foreign_key: :submitter_id,
       class_name: :User
+
+
+
+  #factory method
+  def self.create_for_user_and_long_url!(user,long_url)
+    ShortenedUrl.create!(
+      submitter_id: user_id,
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code
+    )
+  end
+  
+
+  def self.random_code
+    loop do
+      random_code = SecureRandom.urlsafe_base64(16)
+      return random_code unless ShortenedUrl.exists?(short_url: random_code)
+    end
+  end
+
+
         
 end
